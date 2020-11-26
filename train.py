6f937f908099214ae475eac4436b534330e0ba9d
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-11-26 17:14:49 +0000

    kld to mean

--- train.py
+++ train.py
@@ -269,7 +269,7 @@ def train(train_loader, model, logic_net,
             recon_loss = criterion(output, target)
             loss = 0
             loss += recon_loss
-            kld = -0.5 * (1 + lv - np.log(9.) - (mu.pow(2) + lv.exp()) / 9.).sum(dim=1).mean()
+            kld = -0.5 * (1 + lv - np.log(9.) - (mu.pow(2) + lv.exp()) / 9.).mean()
             loss += kld
 
             preds, true = calc_logic(output, target)
