eaa9f5d68704a2c9f19f92ed3f97f4ec77d69ad8
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-11-26 12:38:25 +0000

    definitely override the optimizer

--- train.py
+++ train.py
@@ -166,12 +166,12 @@ def main():
         calc_logic = lambda predictions, targets: calc_logic_loss(predictions, targets, logic_net, logic_fn, device)
 
         # override the oprimizer from above
-        # optimizer = torch.optim.SGD(model.local_parameters,
-        #                             args.lr,
-        #                             momentum=args.momentum,
-        #                             nesterov=args.nesterov,
-        #                             weight_decay=args.weight_decay)
-        # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(train_loader) * args.epochs)
+        optimizer = torch.optim.SGD(model.local_parameters,
+                                    args.lr,
+                                    momentum=args.momentum,
+                                    nesterov=args.nesterov,
+                                    weight_decay=args.weight_decay)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(train_loader) * args.epochs)
 
     for epoch in range(args.start_epoch, args.epochs):
         # train for one epoch
