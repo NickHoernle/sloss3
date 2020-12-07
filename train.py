bd91878abb037d99f1c8fd954b8e298edc511a57
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-12-07 14:31:27 +0000

    trin logic net as much as possible

--- train.py
+++ train.py
@@ -258,24 +258,24 @@ def train_logic_step(model, logic_net, calc_logic, examples, logic_optimizer, de
         logic_net.eval()
         model.train()
 
-        # train the network to obey the logic
-        decoder_optimizer.zero_grad()
+    # train the network to obey the logic
+    decoder_optimizer.zero_grad()
 
-        samps, tgts, thet = model.sample(5000)
-        preds, true = calc_logic(samps, tgts)
-        logic_loss_ = F.binary_cross_entropy_with_logits(preds, torch.ones_like(preds), reduction="none")
+    samps, tgts, thet = model.sample(5000)
+    preds, true = calc_logic(samps, tgts)
+    logic_loss_ = F.binary_cross_entropy_with_logits(preds, torch.ones_like(preds), reduction="none")
 
-        loss = 0
-        # loss = params.sloss_weight*logic_loss_.mean()
-        loss = F.mse_loss(samps, examples[tgts], reduction="none")[~true].sum() / len(true)
-        loss += F.cross_entropy(samps, tgts, reduction="none")[true].sum() / len(true)
-        loss += params.sloss_weight*logic_loss_.mean()
+    loss = 0
+    # loss = params.sloss_weight*logic_loss_.mean()
+    loss = F.mse_loss(samps, examples[tgts], reduction="none")[~true].sum() / len(true)
+    loss += F.cross_entropy(samps, tgts, reduction="none")[true].sum() / len(true)
+    loss += params.sloss_weight*logic_loss_.mean()
 
-        print(true.float().mean(), len(true), loss)
+    print(true.float().mean(), len(true), loss)
 
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.net.parameters(), 5.)
-        decoder_optimizer.step()
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(model.net.parameters(), 5.)
+    decoder_optimizer.step()
 
     # logic_scheduler.step()
     # decoder_scheduler.step()
@@ -300,10 +300,11 @@ def train(train_loader, model, logic_net,
 
     end = time.time()
 
-    net_logic_loss, logic_loss = train_logic_step(model, logic_net, calc_logic, examples,
-                                                  logic_optimizer, decoder_optimizer, logic_scheduler,
-                                                  decoder_scheduler,
-                                                  params, device=device)
+    if params.sloss:
+        net_logic_loss, logic_loss = train_logic_step(model, logic_net, calc_logic, examples,
+                                                      logic_optimizer, decoder_optimizer, logic_scheduler,
+                                                      decoder_scheduler,
+                                                      params, device=device)
 
     logic_losses.update(logic_loss.data.item(), 5000)
     net_logic_losses.update(net_logic_loss.data.item(), 5000)
