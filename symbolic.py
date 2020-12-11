4b0f8a94388dfafb7e34365a01fd3975566e6dce
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-12-11 16:21:39 +0000

    last attempt

--- symbolic.py
+++ symbolic.py
@@ -223,7 +223,7 @@ def get_cifar10_experiment_params(dataset):
 #     return torch.stack([target == tgt] + [((predictions[:, outside_group_ix] + epsilon) < predictions[:, i].unsqueeze(1)).all(dim=1) for i in within_group_ix], dim=1).all(dim=1)
 
 def build_logic(target, predictions, tgt, within_group_ix, outside_group_ix, epsilon=1.):
-    return torch.stack([target == tgt] + [(predictions[:, outside_group_ix] <= -3).all(dim=1)] + [(predictions[:, within_group_ix] >= -2).all(dim=1)], dim=1).all(dim=1)
+    return torch.stack([target == tgt] + [(predictions[:, outside_group_ix] <= 0).all(dim=1)] + [(predictions[:, within_group_ix] >= 2).all(dim=1)], dim=1).all(dim=1)
 
 
 def create_cifar100_logic(group_ixs):
@@ -266,13 +266,13 @@ def get_cifar100_experiment_params(dataset):
         super_class_ix.append([i for i, l in enumerate(classes) if superclass_mapping[l] == sc_l])
 
     examples = torch.ones(100, 100)
-    examples *= -10
+    examples *= -1
 
     for group in super_class_ix:
         for ix in group:
-            examples[ix, group] = -1
+            examples[ix, group] = 2
 
-    examples[torch.arange(100), torch.arange(100)] = 1
+    examples[torch.arange(100), torch.arange(100)] = 10
 
     return examples, create_cifar100_logic(super_class_ix), create_cifar100_group_precision(super_class_ix)
 
